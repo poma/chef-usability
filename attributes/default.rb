75266aee65a396220c151ac58c457c3d661de90a
poma <Semenov.Roman@mail.ru>
2016-08-09 13:49:27 +0300

    packages, timezone

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,1 +1,1 @@
-default['usability']['packages'] = %w{ne tree git screen curl}
+default['usability']['packages'] = %w{ne tree git screen curl man-db telnet lsof zip unzip bash-completion traceroute}
--- recipes/default.rb
+++ recipes/default.rb
@@ -6,3 +6,17 @@ include_recipe "apt"
 node['usability']['packages'].each do |a_package|
   package a_package
 end
+
+link "/etc/localtime" do
+  to "/usr/share/zoneinfo/Europe/Moscow"
+end
+
+cookbook_file "/etc/timezone"
+
+package "tzdata" do
+  action :upgrade
+end
+
+cookbook_file "/usr/share/ne/syntax/xml.jsf" do
+  source "ne.xml.conf"
+end
