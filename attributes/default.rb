97797b0efc8810fb48ac597671f5c047bd7e721e
Roman Semenov <Semenov.Roman@mail.ru>
2017-08-30 16:51:42 +0300

    Update default.rb

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
-default['usability']['packages'] = %w{ne tree git screen curl man-db telnet lsof zip unzip bash-completion traceroute}
+default['usability']['packages'] = %w{ne tree git screen curl man-db telnet lsof zip unzip bash-completion traceroute htop}
 default['usability']['zsh']['plugins'] = "git zsh-autosuggestions zsh-syntax-highlighting zsh-completions command-not-found"
 default['usability']['zsh']['includes'] = []
 default['usability']['root']['default-user'] = 'ubuntu'
